c5d38996e267a4159ce3219d41d8a2d294ac4f55
Pavlos Hatzigeorgiou <82758435+Perlaphim@users.noreply.github.com>
2024-09-19 12:15:59 +0300

    Delete src/gr/ergasies directory

--- src/gr/ergasies/erg16/ask1/IShape.java
+++
@@ -1,5 +0,0 @@
-package gr.ergasies.erg16.ask1;
-
-public interface IShape {
-    long getId();
-}
--- src/gr/ergasies/erg16/ask1/ITwoDimensional.java
+++
@@ -1,5 +0,0 @@
-package gr.ergasies.erg16.ask1;
-
-public interface ITwoDimensional {
-    double getArea();
-}
